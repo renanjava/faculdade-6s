2f76ab5585371ca038a5b2c4b2c8c967f69c0082
renanjava <renancvr777@gmail.com>
2024-10-22 14:54:38 -0300

    ticket de suporte 01 e 02

--- manutencao-cidao/atv01-sem02/ticket-desenvolvimento/ticket02.py
+++ manutencao-cidao/atv01-sem02/ticket-desenvolvimento/ticket02.py
@@ -3,13 +3,16 @@ class BancoDeDados:
         print(f'usuario {usuario} verificado')
 
 class Abstracao:
-    banco_de_dados = BancoDeDados()
+    def __init__(self):
+        self.banco_de_dados = BancoDeDados()
 
     def autenticacao(self, usuario, senha):
         self.banco_de_dados.verificar_usuario(usuario, senha)
 
 class Autenticacao:
-    abstracao = Abstracao()
+    def __init__(self):
+        self.abstracao = Abstracao() 
+
     def autenticar(self, usuario, senha):
         self.abstracao.autenticacao(usuario, senha)
     
